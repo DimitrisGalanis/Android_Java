72803cebb3a878a30977071c5a7195ef2d349be3
Noic Noic <galanisd@csd.auth.gr>
2022-06-02 15:22:40 +0300

    added sign up form

--- app/src/main/java/com/example/aeks/Signup.java
+++ app/src/main/java/com/example/aeks/Signup.java
@@ -1,4 +1,14 @@
 package com.example.aeks;
 
-public class Signup {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
 }
