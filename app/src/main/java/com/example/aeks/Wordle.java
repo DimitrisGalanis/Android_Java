71f1c88ec6d2959a537d18676ab9fb8672e26914
Noic Noic <galanisd@csd.auth.gr>
2022-06-06 17:09:27 +0300

    windows popup added

--- app/src/main/java/com/example/aeks/Wordle.java
+++ app/src/main/java/com/example/aeks/Wordle.java
@@ -2,15 +2,19 @@ package com.example.aeks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -19,7 +23,7 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 public class Wordle extends AppCompatActivity {
-
+    int tries=0;
     EditText Et11, Et12, Et13, Et14, Et15;
     EditText Et21, Et22, Et23, Et24, Et25;
     EditText Et31, Et32, Et33, Et34, Et35;
@@ -27,12 +31,14 @@ public class Wordle extends AppCompatActivity {
     EditText Et51, Et52, Et53, Et54, Et55;
     EditText Et61, Et62, Et63, Et64, Et65;
     static myWrapper wrap = new myWrapper();
-
+    Dialog winDialog,looseDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wordle);
+        winDialog = new Dialog(this);
+        looseDialog = new Dialog(this);
 
         EditText[] array = new EditText[31];
         array[0] = null;
@@ -72,6 +78,7 @@ public class Wordle extends AppCompatActivity {
         array[29] = Et64 = findViewById(R.id.word6_4);
         array[30] = Et65 = findViewById(R.id.word6_5);
 
+
         //TODO Get Word from DB
         String test="ABCDE";
 
@@ -87,7 +94,7 @@ public class Wordle extends AppCompatActivity {
                     @Override
                     public void onResponse(String response) {
                         wrap.setMywordle(response);
-                        Toast.makeText(Wordle.this, wrap.getMywordle() , Toast.LENGTH_SHORT).show();
+                        Toast.makeText(Wordle.this, wrap.getMywordle() , Toast.LENGTH_SHORT).show(); // Appears word
                     }
                 }, new Response.ErrorListener() {
             @Override
@@ -98,7 +105,6 @@ public class Wordle extends AppCompatActivity {
 
         // Add the request to the RequestQueue.
         queue.add(stringRequest);
-        Toast.makeText(Wordle.this, wrap.getMywordle() , Toast.LENGTH_SHORT).show();
 
         for (int i=1 ; i < array.length-5 ; ++i)
         {
@@ -125,7 +131,8 @@ public class Wordle extends AppCompatActivity {
                     }
                     if ((finalI>=5 && finalI%5==0))
                     {
-                        testingLetters(finalI, array, wrap.getMywordle());
+                        ++tries;
+                        testingLetters(finalI, array, wrap.getMywordle() , tries);
                     }
                 }
             });
@@ -134,23 +141,87 @@ public class Wordle extends AppCompatActivity {
 
 
     // Helper Function for testing letters
-    private void testingLetters(int index , EditText[] v , String test){
-        boolean flag ;
-        for ( int j = 0 ; j < test.length() ; ++j)
+    private void testingLetters(int index , EditText[] v , String test , int tries)
+    {
+        int count = 0;
+        for (int i=0 ; i < 5 ; i++)
         {
-            String temp = Character.toString(test.charAt(j));
-            flag = false;
-            for (int i = 0; i < 5 ; ++i)
+            if ( test.contains(v[index +i - 4].getText().toString()) ) // checks if contains and turns yellow
+                v[index +i - 4].getBackground().setColorFilter(Color.rgb(214, 214, 32), PorterDuff.Mode.SRC_ATOP);      // yellow
+
+
+            String temp = Character.toString(test.charAt(i));  // character at i position
+            if ( v[index + i - 4].getText().toString().equals(temp) )  // checks if its in right spot and turns green
             {
-                if ( v[index + i - 4].getText().toString().equals(temp) && i!=j && flag)
-                {
-                    v[index + i - 4].getBackground().setColorFilter(Color.rgb(214, 214, 32), PorterDuff.Mode.SRC_ATOP);     // yellow
-                } else if ( v[index + i - 4].getText().toString().equals(temp) && i==j)
-                {
-                    v[index + i - 4].getBackground().setColorFilter(Color.rgb(56, 120, 24), PorterDuff.Mode.SRC_ATOP);      //  green
-                    flag = true;
-                }
+                v[index + i - 4].getBackground().setColorFilter(Color.rgb(56, 120, 24), PorterDuff.Mode.SRC_ATOP);      //  green
+                ++count;
             }
         }
+        if (count==5)
+        {
+            winDialog.setContentView(R.layout.costum_popup);
+            winDialog.show();
+            Button playAgain = (Button) winDialog.findViewById(R.id.ID_playBTN);
+            TextView x = (TextView) winDialog.findViewById(R.id.finish);
+
+            x.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    winDialog.dismiss();
+                }
+            });
+
+
+            playAgain.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    winDialog.dismiss();
+                    finish();
+                    redirectWordle();
+                }
+            });
+        }
+
+        if (tries==5)
+        {
+            looseDialog.setContentView(R.layout.fail_popup);
+            looseDialog.show();
+            Button playAgain_L = (Button) looseDialog.findViewById(R.id.ID_playBTN_L);
+            TextView x_L = (TextView) looseDialog.findViewById(R.id.finish_L);
+
+            x_L.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    looseDialog.dismiss();
+                }
+            });
+
+
+            playAgain_L.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    looseDialog.dismiss();
+                    finish();
+                    redirectWordle();
+                }
+            });
+
+
+            Et61.setText(Character.toString(wrap.getMywordle().charAt(0)));
+            Et61.getBackground().setColorFilter(Color.rgb(229, 50, 50), PorterDuff.Mode.SRC_ATOP);
+            Et62.setText(Character.toString(wrap.getMywordle().charAt(1)));
+            Et62.getBackground().setColorFilter(Color.rgb(229, 50, 50), PorterDuff.Mode.SRC_ATOP);
+            Et63.setText(Character.toString(wrap.getMywordle().charAt(2)));
+            Et63.getBackground().setColorFilter(Color.rgb(229, 50, 50), PorterDuff.Mode.SRC_ATOP);
+            Et64.setText(Character.toString(wrap.getMywordle().charAt(3)));
+            Et64.getBackground().setColorFilter(Color.rgb(229, 50, 50), PorterDuff.Mode.SRC_ATOP);
+            Et65.setText(Character.toString(wrap.getMywordle().charAt(4)));
+            Et65.getBackground().setColorFilter(Color.rgb(229, 50, 50), PorterDuff.Mode.SRC_ATOP);
+        }
+    }
+
+    private void redirectWordle(){
+        Intent intent = new Intent(this,Wordle.class);
+        startActivity(intent);
     }
 }
